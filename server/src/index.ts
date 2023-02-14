57a7cd9848a21784319a0ff58206c8202728ca12
Hussnain Javed <hussnain@bracketsltd.email>
2023-02-15 00:46:26 +0500

    post deck completed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,49 +1,85 @@
-import  express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import Deck from './models/Deck'
+import cors from 'cors'
 
 const app = express()
 const PORT = 5000
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
+app.use(cors({
+    origin: "*"
+}))
 
 
-app.get(`/`, (req:Request,res: Response)=> {
+app.get(`/`, (req: Request, res: Response) => {
     res.status(200).send(`Server is Healthy`)
-    
+
 })
 
 
-app.get(`/deck/:deckId`, (req:Request,res: Response)=> {
-    const {deckId} = req.params
+app.get(`/deck`, async (req: Request, res: Response) => {
+    try {
+        const deckData = await Deck.find()
+        res.status(200).json({ hasError: false, data: deckData })
+    } catch (err) {
+        if (err) {
+            res.status(500).json({ hasError: true, message: `Internal Server Error` })
+        }
+    }
+
+})
+
+
+
+app.get(`/deck/:deckId`, (req: Request, res: Response) => {
+    const { deckId } = req.params
     // Deck.find({}).then(data=> console.log(data))
     Deck.findById(deckId).exec((err, deck) => {
-        if(err){
-          return  res.status(500).json({hasError: true, error: {message:`internal Server Error has been occurred `}})
-        }else if(!deck){
-          return  res.status(404).json({hasError: true, error: {message:`Not Found`}})
+        if (err) {
+            return res.status(500).json({ hasError: true, error: { message: `internal Server Error has been occurred ` } })
+        } else if (!deck) {
+            return res.status(404).json({ hasError: true, error: { message: `Not Found` } })
         }
         res.status(200).json(deck)
     })
-    
 })
 
-app.post(`/deck`, async (req:Request, res:Response)=> {
-    const {title, author} = req.body
- const newDeck = new Deck({
-    title, 
-    author
- })
+app.delete(`/deck/:deckId`, (req, res) => {
+    const { deckId } = req.params
+    Deck.findByIdAndDelete(deckId).exec((err, deck) => {
+        if (err) {
+            return res.status(500).json({ hasError: true, message: `Internal Server Error` })
+        } else if (!deck) {
+            return res.status(404).json({ hasError: true, message: `Not Found` })
+        }
 
- const createDeck = await newDeck.save() 
- const {title:_title, author:_author} = createDeck
- 
- res.status(201).json({hasError: false, data: {_title, _author}})
+        res.status(200).json({ hasError: false, data: deck })
+    })
+})
+
+app.post(`/deck`, async (req: Request, res: Response) => {
+    try {
+        const { title, author } = req.body
+        const createDeck = new Deck({
+            title,
+            author,
+            test: `hefjejf kwebfkwejfkwjbefkwjebf`
+        })
+
+        const createdDeck = await createDeck.save()
+        const { title: _title, author: _author } = createdDeck
+
+        res.status(201).json({ hasError: false, data: { _title, _author } })
+    }
+    catch {
+        res.status(500).json({ hasError: true, message: `Something went wrong` })
+    }
 
 })
 
 mongoose.set("strictQuery", false);
-mongoose.connect(`mongodb+srv://hussnainjav3d:ffTBfjjSwqluxr7x@deck-card.wajwo3m.mongodb.net/?retryWrites=true&w=majority`).then(()=> {
-    app.listen(PORT, ()=> console.log(`Server is listen on ${PORT}`))
+mongoose.connect(`mongodb+srv://hussnainjav3d:ffTBfjjSwqluxr7x@deck-card.wajwo3m.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    app.listen(PORT, () => console.log(`Server is listen on ${PORT}`))
 })
